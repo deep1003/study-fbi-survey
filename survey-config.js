f801282983dd436955b7d69d2092dc43cbb2990d
Youngsam Chun <deep1003@snu.ac.kr>
2026-07-20 22:45:28 +0900

    Apply confirmatory two-arm survey design

--- survey-config.js
+++ survey-config.js
@@ -1,4 +1,5 @@
 window.FBI_SURVEY_CONFIG = Object.freeze({
   imageRoot: "images/",
+  assignEndpoint: "https://study-fbi-survey-collector.deep1003-pai.workers.dev/assign",
   submitEndpoint: "https://study-fbi-survey-collector.deep1003-pai.workers.dev/submit"
 });
